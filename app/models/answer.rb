5e8f814dd8023d738596a8482a602a77d5ce4436
mdianes <mdianes@gmail.com>
2015-10-22 22:07:12 +0000

    committing changes in first 6 weeks

---
+++ app/models/answer.rb
@@ -0,0 +1,8 @@
+class Answer < ActiveRecord::Base
+    
+        belongs_to :question
+        
+        def gravatar
+        "http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest(email.downcase)}"
+    end
+end
---
+++ app/models/question.rb
@@ -0,0 +1,6 @@
+class Question < ActiveRecord::Base
+        has_many :answers
+        def gravatar
+        "http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest(email.downcase)}"
+    end
+end
